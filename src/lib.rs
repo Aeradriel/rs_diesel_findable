e98cf927cd76f64962e5088f165f1a28c37b8f07
Thibaut Roche <thibaut.roche.perso@gmail.com>
2019-02-14 17:05:40 +0100

    [A] find_all_by func added

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(extern_crate_item_prelude)]
+#![recursion_limit = "128"]
 
 extern crate proc_macro;
 #[macro_use]
@@ -60,6 +60,10 @@ fn gen_find_by_func(
 
         let struct_name = Ident::new(&format!("{}", struct_name), Span::call_site());
         let func_name = Ident::new(&format!("find_by_{}", struct_attribute), Span::call_site());
+        let all_func_name = Ident::new(
+            &format!("find_all_by_{}", struct_attribute),
+            Span::call_site(),
+        );
         let struct_attribute = Ident::new(&struct_attribute, Span::call_site());
         let struct_attribute_col =
             Ident::new(&format!("{}_col", struct_attribute), Span::call_site());
@@ -77,6 +81,12 @@ fn gen_find_by_func(
                         Ok(res) => Some(res),
                         Err(_) => None,
                     }
+                }
+
+                pub fn #all_func_name(attr: & #attr_type, conn: &PgConnection) -> Result<Vec<#struct_name>, ::diesel::result::Error> {
+                    use crate::schema::#table_name::dsl::#struct_attribute as #struct_attribute_col;
+
+                    #table_name::table.filter(#struct_attribute_col.eq(attr)).get_results(conn)
                 }
             }
         };
@@ -96,7 +106,7 @@ fn get_table_name(input: String) -> String {
     let re = Regex::new(r###"#\[table_name = "(.*)"\]"###).unwrap();
     let table_name_attr = input
         .lines()
-        .skip_while(|line| !line.trim_left().starts_with("#[table_name ="))
+        .skip_while(|line| !line.trim_start().starts_with("#[table_name ="))
         .next()
         .expect("Struct must be annotated with #[table_name = \"...\"]");
 
